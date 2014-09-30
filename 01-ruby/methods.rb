81d7921f78676d572d84ff7040633eb157a4a826
Daniel Tsui <tsui.daniel@gmail.com>
2014-09-30 14:39:46 +1000

    methods/functions

---
+++ 01-ruby/methods.rb
@@ -0,0 +1,53 @@
+
+def defuse_bomb()
+  puts "remove the cover"
+  puts "first cut the red wire"
+  puts "then cut the blue wire"
+end
+
+# defuse_bomb()
+
+def area(length, width)
+  return length * width
+end
+
+# puts area(5, 4)
+
+def volume(length, width, height)
+  area = length * width
+  return volume = area * height
+  puts "hello world haha"
+end
+
+# puts volume(2, 3, 4)
+
+def h1(body)
+  "<h1>#{body}</h1>"
+end
+
+# puts h1("hello world")
+# puts h1 "header"
+
+def prompt(question)
+  puts question
+  gets.chomp
+end
+
+# input = prompt "pretty please guess a number: "
+
+def square(number)
+  number * number
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
